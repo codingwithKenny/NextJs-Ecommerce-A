f57058e2861d3b549c23e09e59001dfec98fc299
Ridwat <okunlolaridwanat@gmail.com>
2024-09-19 22:52:39 +0100

    Upload photo to aws

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,9 @@
+import { PutObjectCommand, S3, S3Client } from '@aws-sdk/client-s3';
 import multiparty from 'multiparty';
+import fs from 'fs'
+import mime from 'mime-types'
+
+const BucketNmame = 'next-ecommerce-ridwat'
 
 export default async function handler(req, res) {
   // Create a new instance of multiparty.Form
@@ -18,10 +23,35 @@ export default async function handler(req, res) {
 
     // Log the fields and files
     console.log('Fields:', fields);
-    console.log('Files:', files.file.length);
+    console.log('Files:', files);
+     const  client = new S3Client({
+      region:"eu-north-1",
+      credentials:{
+        accessKeyId:process.env.S3_Accesskey,
+        secretAccessKey:process.env.S3_Secretaccesskey
+      }
+     })
+     const links = []
 
+     for (const file of files.file){
+      const ext = file.originalFilename.split('.').pop()
+      console.log(ext)
+
+      const newFileName = Date.now() + '.' + ext
+
+      client.send(new PutObjectCommand({
+        Bucket:BucketNmame,
+        Key:newFileName,
+        Body:fs.readFileSync(file.path),
+        ACL:"public-read",
+        ContentType:mime.lookup(file.path)
+       }))
+     }
+       
+     const link = `https://${BucketNmame}.s3.amazonaws.com/${newFileName}`
+        links.push(link)
     // Respond with a success message
-    res.status(200).json({ message: 'Upload successful', fields, files });
+    res.status(200).json({ message: 'Upload successful', links});
   } catch (error) {
     // Handle any errors
     console.error('Error processing form:', error);
